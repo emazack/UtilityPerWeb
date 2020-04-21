e171d398bf556630bd7fda8761b273f6b7c5715a
Emanuele <em.zaccaria@gmail.com>
2020-04-21 11:27:03 +0200

    JS eventDelegation sample

--- myscript.js
+++ myscript.js
@@ -1,4 +1,11 @@
 // dicitura per rendere la pagina pronta a ricevere funzioni e terminologia jQuery
 $(document).ready(function(){
-  
+
+  // serve per riferire un evento ad un elemento in pagina che è stato caricato dopo il caricamento iniziale delle pagina (magari perchè è stato creato)
+  $(ElementoGiaPresenteACuiRiferirsi).on(EventoTriggerante, ElementoCreatoDopoACheScatenaLevento,
+    function(){
+      // QUELLO CHE DEVE SUCCEDERE;
+    }
+  );
+
 });
